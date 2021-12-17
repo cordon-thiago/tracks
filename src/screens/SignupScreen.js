2d3f0003d6baab8deec3af200c79f6cb530ad37d
Thiago Cordon <thiago.c.rodrigues@farfetch.com>
2021-12-17 20:05:38 +0000

    Added signup functionality

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,18 +1,40 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import { View, StyleSheet } from "react-native";
 import { Text, Input, Button } from "react-native-elements";
 import Spacer from "../components/Spacer";
+import { Context as AuthContext } from "../context/AuthContext";
 
 const SignupScreen = ({ navigation }) => {
+    const { state, signup } = useContext(AuthContext);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
     return (
         <View style={styles.container}>
             <Spacer>
                 <Text h3>Sign Up for Tracker</Text>
             </Spacer>
-            <Input label="Email" />
-            <Input label="Password" />
+            <Input 
+                label="Email"
+                value={email}
+                onChangeText={setEmail}
+                autoCapitalize="none"
+                autoCorrect={false}
+            />
+            <Input 
+                label="Password"
+                value={password}
+                onChangeText={setPassword}
+                autoCapitalize="none"
+                autoCorrect={false}
+                secureTextEntry
+            />
+            {state.errorMessage ? <Text style={styles.errorMessage}>{state.errorMessage}</Text> : null}
             <Spacer>
-                <Button title="Sign Up"/>
+                <Button 
+                    title="Sign Up"
+                    onPress={() => signup({ email, password })}
+                />
             </Spacer>
         </View>
     );
@@ -20,14 +42,20 @@ const SignupScreen = ({ navigation }) => {
 
 SignupScreen.navigationOptions = () => {
     return {
-      headerShown: false,
+      headerShown: false
     };
 };
 
 const styles = StyleSheet.create({
     container:{
         flex: 1,
-        justifyContent: "center"
+        justifyContent: "center",
+        marginBottom: 250
+    },
+    errorMessage: {
+        fontSize: 16,
+        color: "red",
+        marginLeft: 15
     }
 });
 
