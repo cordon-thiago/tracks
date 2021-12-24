3c234d3d5ae2bae38cc4a829719bcaef4d944802
Thiago Cordon <thiago.c.rodrigues@farfetch.com>
2021-12-24 20:14:51 +0000

    Added Map component

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,13 +1,14 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { StyleSheet } from "react-native";
+import { Text } from "react-native-elements";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Map from "../components/Map";
 
 const TrackCreateScreen = () => {
     return (
         <SafeAreaView forceInset={{ top: "always" }}>
-            <Text style={{ fontSize: 48 }}>
-                TrackCreateScreen
-            </Text>
+            <Text h2>Create a Track</Text>
+            <Map />
         </SafeAreaView>
     );
 };
