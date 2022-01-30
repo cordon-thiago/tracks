05ab5416de3feef1fe327914fe3b7fced9a4d35f
Thiago Cordon <thiaguscr@gmail.com>
2022-01-30 19:17:02 +0000

    Add Location feature

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://78f4-2804-431-cfc8-6a1d-2158-b360-cca2-564.ngrok.io"
+    baseURL: "http://6c70-89-155-35-41.ngrok.io"
 });
